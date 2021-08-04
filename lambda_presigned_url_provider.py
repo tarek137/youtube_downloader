2730bf6de1507138b289160a3f73b1534c959165
tarek137 <tarek.lazaar@gmail.com>
2021-08-04 17:59:07 +0100

    'done'

---
+++ lambda_presigned_url_provider.py
@@ -0,0 +1,38 @@
+
+import json
+import boto3
+
+
+
+def lambda_handler(event, context):
+
+    # Create S3 client and specify region and config
+    s3 = boto3.client('s3' , region_name = "eu-west-3" ,
+                      config=boto3.session.Config(s3={'addressing_style': 'path'}, signature_version='s3v4'))
+    # Create SNS client
+    sns = boto3.client('sns')
+
+    bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    key = event["Records"][0]["s3"]["object"]["key"]
+
+    # generate the presigned url
+
+    generated_url = s3.generate_presigned_url('get_object',
+                                              Params={'Bucket': bucket,
+                                                      'Key': key},
+                                              ExpiresIn=3600)
+
+    # sending the presigned url
+
+    response = sns.publish(
+        TargetArn= "arn:aws:sns:eu-west-3:500974137983:topic-video-url",
+        Message=json.dumps(generated_url)
+    )
+
+
+
+    print(event)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
---
+++ lambda_youtube_download.py
@@ -0,0 +1,28 @@
+#importing the pytube package
+import pytube
+import json
+import boto3
+import os
+
+def lambda_handler(event, context):
+    #create a client for S3 access
+    s3_client = boto3.client('s3' , region_name = "eu-west-3" ,
+                  config=boto3.session.Config(s3={'addressing_style': 'path'}, signature_version='s3v4'))
+
+    #creating a pytube object called youtube from the given URL
+    url = 'https://www.youtube.com/watch?v=FNlac6rTBl4'
+    youtube = pytube.YouTube(url)
+
+    #creating a name variable using the youtube object property "title"
+    #for saving it later
+    name = youtube.title.replace(" ","-").lower()
+
+    #chossing the stream as youtube videos are available in different streams
+    #e.g. different resolutions, etc.
+    video = youtube.streams.first()
+
+    #finally downloading the youtube video and saving it to specified path / name
+    video.download(output_path="/tmp", filename=name)
+
+    fname=os.listdir("/tmp")[0]
+    response = s3_client.upload_file("/tmp/"+fname, "processed-youtube-videos", name+".mp4")
